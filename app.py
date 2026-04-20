2db4f451a173400a024a020dafe7de3e1440b369
paak1010 <onlu8243@gmail.com>
2026-04-20 16:46:03 +0900

    Update app.py

--- app.py
+++ app.py
@@ -127,8 +127,8 @@ if uploaded_file is not None:
             df_grouped = df_grouped.rename(columns={'ME코드': '상품코드'})
             
             final_columns = [
-                '수주일자', '납품일자, '발주코드', '배송코드', '센터', 
-                '수주일자', '납품일자', '상품코드', '품명', 
+                '수주일자', '납품일자, '발주코드', '배송코드', 
+                '센터', '상품코드', '품명', 
                 'UNIT수량', 'UNIT단가', 'Total Amount', '   '
             ]
             
